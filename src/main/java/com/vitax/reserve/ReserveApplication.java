ecf01602af1d0aff549f85188acc16c75a0081c7
Vitor Malevichi <vmalevichi@gmail.com>
2025-12-11 09:48:39 -0300

    First Commit - Create Project

---
+++ src/main/java/com/vitax/reserve/ReserveApplication.java
@@ -0,0 +1,13 @@
+package com.vitax.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitax/reserve/ReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitax.reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
